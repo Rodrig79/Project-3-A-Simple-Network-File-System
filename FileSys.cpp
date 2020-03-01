76d3e60e2027bde4d0a42613f16447c339d3f580
ndoub <60121943+ndoub@users.noreply.github.com>
2020-03-01 01:43:46 -0800

    Update FileSys.cpp

--- FileSys.cpp
+++ FileSys.cpp
@@ -15,7 +15,6 @@ using namespace std;
 
 // mounts the file system
 void FileSys::mount(int sock) {
-	cout << "file system using socket of: " << sock << endl;
   bfs.mount();
   curr_dir = 1; //by default current directory is home directory, in disk block #1
   bfs.read_block(curr_dir, (void*) &curr_dir_block);
@@ -28,25 +27,23 @@ void FileSys::unmount() {
   close(fs_sock);
 }
 
-// TODO: directory is full error
 // make a directory
 void FileSys::mkdir(const char *name)
 {
-	cout << "running mkdir " << name << endl;
 	short newBlock;
 	dirblock_t newDir;
 	
 	if(get_block_index(name) >= 0){
-		send_message(ERR_502);
+		send_response(ERR_502, "");
 		return;
 	} if(strlen(name) > MAX_FNAME_SIZE){
-		send_message(ERR_504);
+		send_response(ERR_504, "");
 		return;
 	} if(curr_dir_block.num_entries == MAX_DIR_ENTRIES){
-		send_message(ERR_506);
+		send_response(ERR_506, "");
 		return;
 	} if(!(newBlock = bfs.get_free_block())){
-		send_message(ERR_505);
+		send_response(ERR_505, "");
 		return;
 	}
 	
@@ -54,6 +51,7 @@ void FileSys::mkdir(const char *name)
 	newDir.magic = DIR_MAGIC_NUM;
 	bfs.write_block(newBlock, &newDir);
 	
+	//copy name of file into directory
 	int i = 0;
 	for(i = 0; name[i] != '\0'; i++)
 		curr_dir_block.dir_entries[curr_dir_block.num_entries].name[i] = name[i];
@@ -62,106 +60,121 @@ void FileSys::mkdir(const char *name)
 	curr_dir_block.dir_entries[curr_dir_block.num_entries].block_num = newBlock;
 	curr_dir_block.num_entries++;
 	bfs.write_block(curr_dir, &curr_dir_block);
-	send_message("OKAY");
+	
+	send_response(STAT_200, "success");
 }
 
-// TODO: entry not found response
-// TODO cstring equality?
 // switch to a directory
 void FileSys::cd(const char *name)
 {
 	dirblock_t new_dir_block;
 	short i;
+	
 	if((i = get_block_index(name)) == -1){
-		send_message(ERR_503);
+		send_response(ERR_503, "");
 		return;
 	}
+	
 	bfs.read_block(curr_dir_block.dir_entries[i].block_num, (void*) &new_dir_block);
-	cout << "moving to: " << curr_dir_block.dir_entries[i].name << endl;
-	cout << new_dir_block.magic << " " << DIR_MAGIC_NUM << endl;
 	if(new_dir_block.magic != DIR_MAGIC_NUM){
-		send_message(ERR_500);
+		send_response(ERR_500, "");
 		return;
 	}
+	
 	curr_dir = curr_dir_block.dir_entries[i].block_num;
 	curr_dir_block = new_dir_block;
-	send_message("OKAY");
+	send_response(STAT_200, "success");
 }
 
 // switch to home directory
 void FileSys::home() {
 	curr_dir = 1;
 	bfs.read_block(curr_dir, (void*) &curr_dir_block);
-	send_message("OKAY");
+	send_response(STAT_200, "success");
 }
 
-//TODO: make sure directory is empty
 // remove a directory
 void FileSys::rmdir(const char *name)
 {
 	short i;
 	dirblock_t rem_block;
 	if((i = get_block_index(name)) == -1){
-		send_message(ERR_503);
+		send_response(ERR_503, "");
 		return;
 	}
 	short rem = curr_dir_block.dir_entries[i].block_num;
 	bfs.read_block(rem, (void*) &rem_block);
 	if(rem_block.magic != DIR_MAGIC_NUM){
-		send_message(ERR_500);
+		send_response(ERR_500, "");
 		return;
 	}
 	if(rem_block.num_entries > 0){
-		send_message(ERR_507);
+		send_response(ERR_507, "");
 		return;
 	}
-	swap(curr_dir_block.dir_entries[rem], curr_dir_block.dir_entries[curr_dir_block.num_entries - 1]);
+	//move directory to remove to the end of the list then decrease size of list
+	swap(curr_dir_block.dir_entries[i], curr_dir_block.dir_entries[curr_dir_block.num_entries - 1]);
 	curr_dir_block.num_entries--;
 	bfs.write_block(curr_dir, (void*) &curr_dir_block);
 	bfs.reclaim_block(rem);
-	send_message("OKAY");
+	send_response(STAT_200, "success");
 }
 
 // list the contents of current directory
-//TODO: remove extra space at end
 void FileSys::ls()
 {
-	cout << "running ls" << endl;
-	string file_list = "*";
-	for(int i = 0; i < curr_dir_block.num_entries; i++){
-		file_list.append(curr_dir_block.dir_entries[i].name);
-		file_list += "*";
+	string file_list = "";
+	
+	//if nothing in directory return empty
+	if(curr_dir_block.num_entries == 0){
+		send_response(STAT_200, "empty folder");
+		return;
 	}
-	cout << "result of ls is: " << file_list << endl;
-	send_message(file_list);
+	
+	dirblock_t temp;
+	for(int i = 0; i < curr_dir_block.num_entries - 1; i++){
+		file_list.append(curr_dir_block.dir_entries[i].name);
+		bfs.read_block(curr_dir_block.dir_entries[i].block_num, (void*) &temp);
+		if(temp.magic == DIR_MAGIC_NUM)
+			file_list.append("/");
+		file_list += " ";
+	}
+	file_list.append(curr_dir_block.dir_entries[curr_dir_block.num_entries - 1].name);
+	bfs.read_block(curr_dir_block.dir_entries[curr_dir_block.num_entries - 1].block_num, (void*) &temp);
+		if(temp.magic == DIR_MAGIC_NUM)
+	file_list.append("/");
+	
+	send_response(STAT_200, file_list);
 }
-// TODO: error message if full
+
 // create an empty data file
 void FileSys::create(const char *name)
 {
 	inode_t new_file;
 	short new_block;
-	cout << get_block_index(name) << endl;
+	
 	if(get_block_index(name) >= 0){
-		send_message(ERR_502);
+		send_response(ERR_502, "");
 		return;
 	}
 	if(strlen(name) > MAX_FNAME_SIZE){
-		send_message(ERR_504);
+		send_response(ERR_504, "");
 		return;
 	}
 	if(curr_dir_block.num_entries == MAX_DIR_ENTRIES){
-		send_message(ERR_506);
+		send_response(ERR_506, "");
 		return;
 	}
 	if(!(new_block = bfs.get_free_block())) {
-		send_message(ERR_505);
+		send_response(ERR_505, "");
 		return;
 	}
+	
 	new_file.size = 0;
 	new_file.magic = INODE_MAGIC_NUM;
 	bfs.write_block(new_block, &new_file);
 	
+	//copy name of file into directory
 	int i = 0;
 	for(i = 0; name[i] != '\0'; i++)
 		curr_dir_block.dir_entries[curr_dir_block.num_entries].name[i] = name[i];
@@ -170,64 +183,73 @@ void FileSys::create(const char *name)
 	curr_dir_block.dir_entries[curr_dir_block.num_entries].block_num = new_block;
 	curr_dir_block.num_entries++;
 	bfs.write_block(curr_dir, (void*) &curr_dir_block);
-	send_message("OKAY");
+	
+	send_response(STAT_200, "success");
 }
 
-// TODO: file doesn't exist, file too big
 // append data to a data file
 void FileSys::append(const char *name, const char *data)
 {
-	cout << "appending: " << data << "to: " << name << endl;
 	short inode;
 	inode_t inode_block;
 	int data_index;
 	int data_offset;
 	datablock_t curr_block;
+	
 	if((inode = get_block_index(name)) == -1) {
-		send_message(ERR_503);
+		send_response(ERR_503, "");
 		return;
 	}
+	
 	bfs.read_block(curr_dir_block.dir_entries[inode].block_num, (void*) &inode_block);
 	if(inode_block.magic != INODE_MAGIC_NUM){
-		send_message(ERR_501);
+		send_response(ERR_501, "");
 		return;
 	}
+	if(inode_block.size == MAX_FILE_SIZE) {
+		send_response(ERR_508, "");
+		return;
+	}
+	//get the position of the last byte in the file
 	data_index = inode_block.size / BLOCK_SIZE;
 	data_offset = inode_block.size % BLOCK_SIZE;
-	int i = 0;
-	if(inode_block.size == 0) {
-		cout << "creating new block for data" << endl;
-		inode_block.blocks[0] = bfs.get_free_block();
+	
+	//allocate new block if last inode block is full
+	if(data_offset == 0) {
+		inode_block.blocks[data_index] = bfs.get_free_block();
 	}
+	
 	bfs.read_block(inode_block.blocks[data_index], (void*) &curr_block);
+	int i = 0;
 	while(data[i] != '\0'){
+		
+		//if at end of current data block check to see if we can get a new one
 		if(data_offset == BLOCK_SIZE) {
 			data_offset = 0;
 			bfs.write_block(inode_block.blocks[data_index++], (void*) &curr_block);
-			//ask which one of these errors to return first
+			
 			if(data_index == MAX_DATA_BLOCKS) {
 				inode_block.size = data_index * BLOCK_SIZE;
 				bfs.write_block(curr_dir_block.dir_entries[inode].block_num, (void*) &inode_block);
-				send_message(ERR_508);
+				send_response(ERR_508, "");
 				return;
 			}
 			if((inode_block.blocks[data_index] = bfs.get_free_block()) == 0) {
 				inode_block.size = data_index * BLOCK_SIZE;
 				bfs.write_block(curr_dir_block.dir_entries[inode].block_num, (void*) &inode_block);
-				send_message(ERR_505);
+				send_response(ERR_505, "");
 				return;
 			}
+			//create new data block to fill
 			curr_block = datablock_t();
 		}
-		cout << "copying: " << data[i] << endl;
 		curr_block.data[data_offset++] = data[i++];
 	}
+	
 	bfs.write_block(inode_block.blocks[data_index], (void*) &curr_block);
-	cout << "data_index: " << data_index << " data_offset: " << data_offset << endl;
 	inode_block.size = data_index * BLOCK_SIZE + data_offset;
-	cout << "inode_block size: " << inode_block.size << endl;
 	bfs.write_block(curr_dir_block.dir_entries[inode].block_num, (void*) &inode_block);
-	send_message("OKAY");
+	send_response(STAT_200, "success");
 }
 
 // display the contents of a data file
@@ -239,25 +261,30 @@ void FileSys::cat(const char *name)
 	int index = 0;
 	int offset = 0;
 	int block_num = 0;
+	
 	if((inode_index = get_block_index(name)) == -1) {
-		send_message(ERR_503);
+		send_response(ERR_503, "");
 		return;
 	}
+	
 	bfs.read_block(curr_dir_block.dir_entries[inode_index].block_num, (void*) &inode_block);
 	if(inode_block.magic != INODE_MAGIC_NUM) {
-		send_message(ERR_501);
+		send_response(ERR_501, "");
 		return;
 	}
+	
 	char file_data[inode_block.size + 1];
 	for(int i = 0; i < inode_block.size; i++) {
+		//if starting a new block, read that block of data
 		if(offset == 0) {
 			bfs.read_block(inode_block.blocks[block_num++], (void*) &data_block);
 		}
 		file_data[index++] = data_block.data[offset++];
 		offset = offset % BLOCK_SIZE;
 	}
+	
 	file_data[index] = '\0';
-	send_message(string(file_data));
+	send_response(STAT_200, string(file_data));
 }
 
 // display the first N bytes of the file
@@ -269,18 +296,23 @@ void FileSys::head(const char *name, unsigned int n)
 	int index = 0;
 	int offset = 0;
 	int block_num = 0;
+	
 	if((inode_index = get_block_index(name)) == -1) {
-		send_message(ERR_503);
+		send_response(ERR_503, "");
 		return;
 	}
+	
 	bfs.read_block(curr_dir_block.dir_entries[inode_index].block_num, (void*) &inode_block);
 	if(inode_block.magic != INODE_MAGIC_NUM) {
-		send_message(ERR_501);
+		send_response(ERR_501, "");
 		return;
 	}
+	// determine if to print whole file or n bytes
 	n = (n > inode_block.size) ? inode_block.size : n;
 	char file_data[n + 1];
+	
 	for(int i = 0; i < n; i++) {
+		//if starting a new block, read that block of data
 		if(offset == 0) {
 			bfs.read_block(inode_block.blocks[block_num++], (void*) &data_block);
 		}
@@ -288,7 +320,7 @@ void FileSys::head(const char *name, unsigned int n)
 		offset = offset % BLOCK_SIZE;
 	}
 	file_data[index] = '\0';
-	send_message(file_data);
+	send_response(STAT_200, file_data);
 }
 
 // delete a data file
@@ -300,26 +332,29 @@ void FileSys::rm(const char *name)
 	int block_num;
 	
 	if((inode_index = get_block_index(name)) == -1) {
-		send_message(ERR_503);
+		send_response(ERR_503, "");
 		return;
 	}
+	
 	inode_block_num = curr_dir_block.dir_entries[inode_index].block_num;
 	bfs.read_block(inode_block_num, (void*) &inode_block);
 	if(inode_block.magic != INODE_MAGIC_NUM) {
-		send_message(ERR_501);
+		send_response(ERR_501, "");
 		return;
 	}
 	
+	//move file to remove to the end of the list then decrease size of list
 	swap(curr_dir_block.dir_entries[inode_index], curr_dir_block.dir_entries[curr_dir_block.num_entries - 1]);
 	curr_dir_block.num_entries--;
 	bfs.write_block(curr_dir, (void*) &curr_dir_block);
-	block_num = (inode_block.size == 0) ? 0 : inode_block.size - 1 / BLOCK_SIZE + 1;
+	block_num = (inode_block.size == 0) ? 0 : ((inode_block.size - 1) / BLOCK_SIZE + 1);
 	
+	//reclaim all datablocks used by file
 	for(int i = 0; i < block_num; i++) {
 		bfs.reclaim_block(inode_block.blocks[i]);
 	}
 	bfs.reclaim_block(inode_block_num);
-	send_message("OKAY");
+	send_response(STAT_200, "success");
 }
 
 void FileSys::stat(const char* name) {
@@ -327,51 +362,63 @@ void FileSys::stat(const char* name) {
 	inode_t i;
 	short index;
 	stringstream message;
+	
 	if((index = get_block_index(name)) == -1) {
-		send_message(ERR_503);
+		send_response(ERR_503, "");
 		return;
 	}
+	
 	bfs.read_block(curr_dir_block.dir_entries[index].block_num, (void*) &d);
 	if(d.magic == DIR_MAGIC_NUM) {
-		message << "File Name:     " << name << "\n";
-		message << "Block Number:     " << curr_dir_block.dir_entries[index].block_num;
+		message << "Directory name:     " << name << "/" << "\n";
+		message << "Directory block:     " << curr_dir_block.dir_entries[index].block_num;
 	} else {
 		bfs.read_block(curr_dir_block.dir_entries[index].block_num, (void*) &i);
-		message << "Inode Block:     " << curr_dir_block.dir_entries[index].block_num << "\n";
-		message << "Bytes in File:     " << i.size << "\n";
-		message << "Number of Blocks:     " << i.size / BLOCK_SIZE << "\n";
-		message << "First Number of Blocks:     " << i.blocks[0];
+		message << "Inode block:     " << curr_dir_block.dir_entries[index].block_num << "\n";
+		message << "Bytes in file:     " << i.size << "\n";
+		if(i.size == 0){
+			message << "Number of blocks:     " << 1 << "\n";
+			message << "First block: " << 0;
+		} else {
+			message << "Number of blocks:     " << (i.size - 1) / BLOCK_SIZE + 2 << "\n";
+			message << "First block:     " << i.blocks[0];
+		}
 	}
-	send_message(message.str());
+	
+	send_response(STAT_200, message.str());
 }
 
 // HELPER FUNCTIONS (optional)
+//returns the index of the file with given name in the current directory
+//returns -1 if no such file exists
 short FileSys::get_block_index(const char* name) {
 	for(int i = 0; i < curr_dir_block.num_entries; i++) {
-		cout << curr_dir_block.dir_entries[i].name << " " << name << endl;
-		cout << strcmp(curr_dir_block.dir_entries[i].name, name) << endl;
-		if(strcmp(curr_dir_block.dir_entries[i].name, name) == 0){
-			cout << "block index is: " << i << endl;
+		if(strcmp(curr_dir_block.dir_entries[i].name, name) == 0)
 			return i;
-		}
 	}
-	cout << "block index is: " << -1 << endl;
 	return -1;
 }
 
-//debug recieving of blank messages, it broke again
+//sends a string to the client
 void FileSys::send_message(string message) {
-	cout << "SENDING MESSAGE: $" << message << "$" << endl;
-	const char* buf = (message + "\r\n").c_str();
+	const char* buf = message.c_str();
 	int len = message.length();
 	int sent_data = 0;
 	int recently_sent_data = 0;
 	while(sent_data < len){
 		recently_sent_data = send(fs_sock, (void*) (buf + sent_data), len - sent_data, 0);
 		if(recently_sent_data == -1) {
-			cout << "error sending data" << endl;
+			cout << "ran into an error while sending data" << endl;
 			break;
 		}
 		sent_data += recently_sent_data;
 	}
+}
+
+//sends the four line format response using send_message
+void FileSys::send_response(string status, string message) {
+	send_message(status + "\r\n");
+	send_message("Length: " + to_string(message.length()) +	 "\r\n");
+	send_message("\r\n");
+	send_message(message);
 }
